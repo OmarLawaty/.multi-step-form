634084026025789a9b68a58f616a4a20d4b82ed5
Omar Lawatey <omarlawatey@gmail.com>
2023-12-20 20:15:46 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+type Step = 'your-info' | 'select-plan' | 'addons' | 'summary';
+
+const STEPS: Step[] = ['your-info', 'select-plan', 'addons', 'summary'];
+
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
