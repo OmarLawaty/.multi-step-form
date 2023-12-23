b8f6155bd63593249a38e0fc763d725343214cf6
Omar Lawatey <omarlawatey@gmail.com>
2023-12-23 23:44:46 +0200

    Refact: Remake all structure types and constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,38 @@
+import { arcadeIcon, controllerIcon, gamePadIcon } from './assets/images';
+
 export const DEFAULT_FORM_VALUES: FormValues = {
   currentStep: 'your_info',
   name: '',
   email: '',
   phoneNumber: '',
   isValid: true,
-  selectedPlan: 0,
+  selectedPlan: 'arcade',
   subscriptionType: 'monthly',
   activeAddons: [true, false, false]
 };
 
 export const STEPS: Step[] = ['your_info', 'select_plan', 'add-ons', 'summary'];
+
+export const PLANS: PlanInfo[] = [
+  { title: 'arcade', cost: 9, icon: arcadeIcon, freeMonths: 2 },
+  { title: 'advanced', cost: 12, icon: gamePadIcon, freeMonths: 2 },
+  { title: 'pro', cost: 15, icon: controllerIcon, freeMonths: 2 }
+];
+
+export const ADDONS: AddonInfo[] = [
+  {
+    title: 'online-service',
+    description: 'Access to multiplayer games',
+    cost: 1
+  },
+  {
+    title: 'larger-storage',
+    description: 'Extra 1TB of cloud save',
+    cost: 2
+  },
+  {
+    title: 'customizable-profile',
+    description: 'Custom theme on your profile',
+    cost: 2
+  }
+];
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,20 +1,30 @@
 type Step = 'your_info' | 'select_plan' | 'add-ons' | 'summary';
+type Plan = 'arcade' | 'advanced' | 'pro';
+type Addon = 'online-service' | 'larger-storage' | 'customizable-profile';
 
-type UserInfo = {
+interface PlanInfo {
+  title: Plan;
+  cost: number;
+  icon: string;
+  freeMonths?: number;
+}
+
+interface AddonInfo {
+  title: Addon;
+  description: string;
+  cost: number;
+}
+
+interface FormValues {
+  currentStep: Step;
+  isValid: boolean;
+
   name: string;
   email: string;
   phoneNumber: string;
-  isValid: boolean;
-};
 
-type PlanInfo = {
-  selectedPlan: 0 | 1 | 2;
-};
-
-type FormInfo = UserInfo & PlanInfo;
-
-interface FormValues extends FormInfo {
-  currentStep: Step;
   subscriptionType: 'monthly' | 'yearly';
+  selectedPlan: Plan;
+
   activeAddons: [boolean, boolean, boolean];
 }
