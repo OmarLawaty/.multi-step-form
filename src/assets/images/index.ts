04edf6c100d11e17a96c16add41c82a1b4c36ec9
Omar Lawatey <omarlawatey@gmail.com>
2023-12-26 08:34:39 +0200

    Feat: Finish navigation panel style and functionality

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,3 +1,6 @@
 export { default as arcadeIcon } from './icon-arcade.svg';
 export { default as gamePadIcon } from './icon-advanced.svg';
 export { default as controllerIcon } from './icon-pro.svg';
+
+export { default as desktopSideBarBg } from './bg-sidebar-desktop.svg';
+export { default as mobileSideBarBg } from './bg-sidebar-mobile.svg';
--- src/components/NavigationPanel/NavigationPanel.tsx
+++ src/components/NavigationPanel/NavigationPanel.tsx
@@ -1,39 +1,64 @@
-import { Box, Flex, Text } from '@chakra-ui/react';
+import { Flex, Text } from '@chakra-ui/react';
 
 import { STEPS } from '../../constants';
 import { camelCaseToNormal } from '../../utils/helpers';
 import { useStepState } from '../../contexts';
+import { desktopSideBarBg, mobileSideBarBg } from '../../assets/images';
 
 export const NavigationPanel = () => {
   const [currentStep] = useStepState();
 
   return (
-    <Box as="article" minW="15vw">
+    <Flex
+      as="article"
+      minW={[null, null, '270px']}
+      h={['30vh', null, 'auto']}
+      flexDir={['row', null, 'column']}
+      align={['flex-start', null, 'center']}
+      justify={['center', null, 'flex-start']}
+      gap={[4, null, 6]}
+      bgImage={[mobileSideBarBg, null, desktopSideBarBg]}
+      bgRepeat="no-repeat"
+      bgSize="100%"
+      px={[null, null, 7]}
+      py={[8, null, 9]}
+    >
       {STEPS.map((step, index) =>
         step === 'thankYou' ? null : (
-          <Flex key={index}>
+          <Flex
+            key={index}
+            align="center"
+            gap="3.5"
+            textTransform="uppercase"
+            color="white"
+            w={['fit-content', null, '100%']}
+          >
             <Flex
               align="center"
               justify="center"
-              w="16"
-              h="16"
+              w={[8, null, 9]}
+              h={[8, null, 9]}
               rounded="full"
               borderWidth="1px"
               borderStyle="solid"
-              borderColor="gray.300"
-              bg={currentStep === step ? 'gray.500' : 'transparent'}
+              borderColor={currentStep === step ? 'blue.200' : 'gray.100'}
+              bg={currentStep === step ? 'blue.200' : 'transparent'}
+              color={currentStep === step ? 'blue.900' : 'white'}
+              fontSize="sm"
             >
               {index + 1}
             </Flex>
 
-            <Flex flexDir="column">
-              <Text>step {index + 1}</Text>
+            <Flex flexDir="column" display={['none', null, 'flex']}>
+              <Text color="gray.500" fontSize="xs" fontWeight="normal">
+                step {index + 1}
+              </Text>
 
               <Text>{camelCaseToNormal(step)}</Text>
             </Flex>
           </Flex>
         )
       )}
-    </Box>
+    </Flex>
   );
 };
