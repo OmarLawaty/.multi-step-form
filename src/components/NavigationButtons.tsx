d0d841a0313b88e0b5bb51a1e0ea2681c37f8c94
Omar Lawatey <omarlawatey@gmail.com>
2023-12-27 00:54:11 +0200

    Style: finish navigation buttons styles

--- src/components/NavigationButtons.tsx
+++ src/components/NavigationButtons.tsx
@@ -17,10 +17,52 @@ type NavigationButtonsProps = {
 );
 
 export const NavigationButtons = ({ isFirstStep, onBack, onNext, isError, children }: NavigationButtonsProps) => (
-  <Flex mt="auto">
-    {!isFirstStep && <Button onClick={() => onBack()}>Go Back</Button>}
+  <Flex
+    mt="auto"
+    pos={['fixed', null, 'static']}
+    insetX="0"
+    bottom="0"
+    bg="white"
+    p="4"
+    px={[4, null, 0.5]}
+    fontWeight="500"
+    fontSize={['sm', null, 'md']}
+  >
+    {!isFirstStep && (
+      <Button
+        bg="transparent"
+        p="0"
+        h="auto"
+        color="gray.500"
+        fontSize="inherit"
+        fontWeight="inherit"
+        transition="color 0.5s"
+        _hover={{
+          bg: 'transparent',
+          color: 'blue.900'
+        }}
+        onClick={() => onBack()}
+      >
+        Go Back
+      </Button>
+    )}
 
-    <Button ml="auto" onClick={() => onNext()} _disabled={{ cursor: 'not-allowed' }} isDisabled={isError}>
+    <Button
+      ml="auto"
+      color="white"
+      bg="blue.900"
+      p={['inherit', null, 6]}
+      fontSize="inherit"
+      fontWeight="inherit"
+      transition="opacity 0.5s"
+      _hover={{
+        opacity: '0.85'
+      }}
+      _active={{}}
+      onClick={() => onNext()}
+      _disabled={{ cursor: 'not-allowed' }}
+      isDisabled={isError}
+    >
       {children}
     </Button>
   </Flex>
