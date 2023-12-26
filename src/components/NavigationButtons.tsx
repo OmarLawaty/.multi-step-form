30a6fef354e6b3cbad48eb163ef078e8194ea39a
Omar Lawatey <omarlawatey@gmail.com>
2023-12-26 02:20:28 +0200

    Refact: Made Navigation Buttons reusable and work as intended

--- src/components/NavigationButtons.tsx
+++ src/components/NavigationButtons.tsx
@@ -1,42 +1,27 @@
+import { ReactNode } from 'react';
 import { Button, Flex } from '@chakra-ui/react';
 
-import { STEPS } from '../constants';
-import { getStep } from '../utils/helpers';
+type NavigationButtonsProps = {
+  children?: ReactNode;
+  onNext: () => void;
+  isError?: boolean;
+} & (
+  | {
+      isFirstStep: true;
+      onBack?: never;
+    }
+  | {
+      isFirstStep?: false;
+      onBack: () => void;
+    }
+);
 
-interface NavigationButtonsProps {
-  formValues: FormValues;
-  setFormValues: React.Dispatch<React.SetStateAction<FormValues>>;
-}
+export const NavigationButtons = ({ isFirstStep, onBack, onNext, isError, children }: NavigationButtonsProps) => (
+  <Flex mt="auto">
+    {!isFirstStep && <Button onClick={() => onBack()}>Go Back</Button>}
 
-export const NavigationButtons = ({ formValues, setFormValues }: NavigationButtonsProps) => {
-  return (
-    <Flex mt="auto">
-      {STEPS.indexOf(formValues.currentStep) > 0 ? (
-        <Button
-          onClick={() =>
-            setFormValues({ ...formValues, isValid: true, currentStep: getStep(formValues.currentStep, -1) })
-          }
-        >
-          Go Back
-        </Button>
-      ) : null}
-
-      <Button
-        ml="auto"
-        onClick={() =>
-          setFormValues({
-            ...formValues,
-            currentStep:
-              STEPS.indexOf(formValues.currentStep) < STEPS.length - 1
-                ? getStep(formValues.currentStep, 1)
-                : formValues.currentStep
-          })
-        }
-        _disabled={{ cursor: 'not-allowed' }}
-        isDisabled={!formValues.isValid}
-      >
-        {STEPS.indexOf(formValues.currentStep) < STEPS.length - 1 ? 'Next Step' : 'Confirm'}
-      </Button>
-    </Flex>
-  );
-};
+    <Button ml="auto" onClick={() => onNext()} _disabled={{ cursor: 'not-allowed' }} isDisabled={isError}>
+      {children}
+    </Button>
+  </Flex>
+);
