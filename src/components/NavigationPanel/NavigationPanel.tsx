a90f62d78498471f064d9963f5cc27a960d4f609
Omar Lawatey <omarlawatey@gmail.com>
2023-12-26 02:23:28 +0200

    Feat: Implement ThankYou step and added case for not showing in the navigation panel

--- src/components/NavigationPanel/NavigationPanel.tsx
+++ src/components/NavigationPanel/NavigationPanel.tsx
@@ -1,37 +1,39 @@
 import { Box, Flex, Text } from '@chakra-ui/react';
 
 import { STEPS } from '../../constants';
+import { camelCaseToNormal } from '../../utils/helpers';
+import { useStepState } from '../../contexts';
 
-interface NavigationPanelProps {
-  currentStep: Step;
-}
+export const NavigationPanel = () => {
+  const [currentStep] = useStepState();
 
-export const NavigationPanel = ({ currentStep }: NavigationPanelProps) => {
   return (
     <Box as="article" minW="15vw">
-      {STEPS.map((step, index) => (
-        <Flex key={index}>
-          <Flex
-            align="center"
-            justify="center"
-            w="16"
-            h="16"
-            rounded="full"
-            borderWidth="1px"
-            borderStyle="solid"
-            borderColor="gray.300"
-            bg={currentStep === step ? 'gray.500' : 'transparent'}
-          >
-            {index + 1}
-          </Flex>
+      {STEPS.map((step, index) =>
+        step === 'thankYou' ? null : (
+          <Flex key={index}>
+            <Flex
+              align="center"
+              justify="center"
+              w="16"
+              h="16"
+              rounded="full"
+              borderWidth="1px"
+              borderStyle="solid"
+              borderColor="gray.300"
+              bg={currentStep === step ? 'gray.500' : 'transparent'}
+            >
+              {index + 1}
+            </Flex>
 
-          <Flex flexDir="column">
-            <Text>step {index + 1}</Text>
+            <Flex flexDir="column">
+              <Text>step {index + 1}</Text>
 
-            <Text>{step.split('_').join(' ')}</Text>
+              <Text>{camelCaseToNormal(step)}</Text>
+            </Flex>
           </Flex>
-        </Flex>
-      ))}
+        )
+      )}
     </Box>
   );
 };
