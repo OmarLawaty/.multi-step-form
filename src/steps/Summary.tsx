45dee5932c1a3b5ec05be555788e0ec6cb5fa63a
Omar Lawatey <omarlawatey@gmail.com>
2023-12-27 07:47:38 +0200

    Style: Finish step no.4 style

--- src/steps/Summary.tsx
+++ src/steps/Summary.tsx
@@ -1,7 +1,9 @@
-import { Box, Flex, Heading, Text } from '@chakra-ui/react';
+import { Divider, Flex, Text } from '@chakra-ui/react';
 
 import { useProductOptionsState, useStepState } from '../contexts';
-import { NavigationButtons } from '../components';
+import { NavigationButtons, StepHeader } from '../components';
+import { Plan } from '../assets/types';
+import { transformString } from '../utils/helpers';
 
 export const Summary = () => {
   const [currentProductOptions, setCurrentProductOptions] = useProductOptionsState();
@@ -10,69 +12,57 @@ export const Summary = () => {
 
   const isMonthly = currentProductOptions.subscription === 'monthly';
 
-  const renderSelectedPlan = () => {
-    const currentPlan = currentProductOptions.plans.getPlan();
-
-    return (
-      <>
-        <Flex>
-          <Text textTransform="capitalize">
-            {currentPlan.title}
-
-            <Text as="span">({currentProductOptions.subscription})</Text>
-          </Text>
-
-          <Text
-            cursor="pointer"
-            textDecor="underline"
-            onClick={() => setCurrentStep({ type: 'set-step', payload: { selectedStep: 'selectPlan' } })}
-          >
-            Change
-          </Text>
-        </Flex>
-
-        <Text>{isMonthly ? `$${currentPlan.cost.monthly}/mo` : `$${currentPlan.cost.yearly}/yr`}</Text>
-      </>
-    );
-  };
-
   return (
     <>
-      <Box>
-        <Heading as="h1">Finishing up</Heading>
+      <StepHeader title="Finishing up" description="Double-check everything looks OK before confirming." />
 
-        <Text>Double-check everything looks OK before confirming.</Text>
-      </Box>
+      <Flex flexDir="column" gap={[4, null, 6]}>
+        <Flex flexDir="column" bg="gray.50" rounded="10" py="5" pl={[4, null, 5]} pr={[4, null, 7]}>
+          <CurrentPlanInfo
+            currentPlan={currentProductOptions.plans.getPlan()}
+            subscription={currentProductOptions.subscription}
+            isMonthly={isMonthly}
+            onPlanChange={() => setCurrentStep({ type: 'set-step', payload: { selectedStep: 'selectPlan' } })}
+          />
 
-      <Flex>{renderSelectedPlan()}</Flex>
+          {!!currentProductOptions.addons.selectedAddons.length && <Divider mt={[3, null, 6]} mb={[3, null, 4]} />}
 
-      <Flex flexDir="column">
-        {currentProductOptions.addons.selectedAddons.map(addonKey => {
-          const addon = currentProductOptions.addons.getAddon(addonKey);
+          <Flex flexDir="column" gap={[3, null, 4]}>
+            {currentProductOptions.addons.selectedAddons.map(addonKey => {
+              const addon = currentProductOptions.addons.getAddon(addonKey);
 
-          return (
-            <Flex key={addon.key}>
-              <Text>{addon.title}</Text>
+              return (
+                <Flex key={addon.key}>
+                  <Text color="gray.500" fontWeight="500" fontSize="sm">
+                    {transformString(addon.title)}
+                  </Text>
 
-              <Text>+${isMonthly ? `${addon.cost.monthly}/mo` : `${addon.cost.yearly}/yr`}</Text>
-            </Flex>
-          );
-        })}
-      </Flex>
+                  <Text ml="auto" color="blue.900" fontWeight="400" fontSize="sm">
+                    +${isMonthly ? `${addon.cost.monthly}/mo` : `${addon.cost.yearly}/yr`}
+                  </Text>
+                </Flex>
+              );
+            })}
+          </Flex>
+        </Flex>
 
-      <Flex>
-        <Text>Total (per {isMonthly ? 'month' : 'year'})</Text>
+        <Flex pl={[4, null, 5]} pr={[4, null, 7]} align="center">
+          <Text color="gray.500" fontWeight="500" fontSize="sm">
+            Total (per {isMonthly ? 'month' : 'year'})
+          </Text>
 
-        <Text>
-          +$
-          {`${currentProductOptions.getTotalCost()}/${isMonthly ? 'mo' : 'yr'}`}
-        </Text>
+          <Text ml="auto" color="purple.800" fontSize={['md', null, 'xl']}>
+            +$
+            {`${currentProductOptions.getTotalCost()}/${isMonthly ? 'mo' : 'yr'}`}
+          </Text>
+        </Flex>
       </Flex>
 
       <NavigationButtons
         onNext={() => {
           // Data to be sent to backend
           console.log(currentProductOptions.getAllDataObject());
+
           setCurrentProductOptions({ type: 'reset-options' });
           setCurrentStep({ type: 'go-to-next-step' });
         }}
@@ -85,3 +75,42 @@ export const Summary = () => {
     </>
   );
 };
+
+interface CurrentPlanInfoProps {
+  currentPlan: Plan;
+  subscription: 'monthly' | 'yearly';
+  isMonthly: boolean;
+  onPlanChange: React.MouseEventHandler<HTMLParagraphElement>;
+}
+
+const CurrentPlanInfo = ({ currentPlan, subscription, onPlanChange, isMonthly }: CurrentPlanInfoProps) => (
+  <Flex align="center" color="blue.900">
+    <Flex flexDir="column">
+      <Text textTransform="capitalize" fontSize={['sm', null, 'md']} lineHeight={['1.2', null, 'normal']}>
+        {currentPlan.title}
+
+        <Text as="span"> ({transformString(subscription)})</Text>
+      </Text>
+
+      <Text
+        color="gray.500"
+        w="fit-content"
+        fontWeight="500"
+        fontSize="sm"
+        cursor="pointer"
+        textDecor="underline"
+        _hover={{
+          color: 'gray.600'
+        }}
+        transition="color 0.5s"
+        onClick={onPlanChange}
+      >
+        Change
+      </Text>
+    </Flex>
+
+    <Text ml="auto" fontSize={['sm', null, 'md']}>
+      {isMonthly ? `$${currentPlan.cost.monthly}/mo` : `$${currentPlan.cost.yearly}/yr`}
+    </Text>
+  </Flex>
+);
